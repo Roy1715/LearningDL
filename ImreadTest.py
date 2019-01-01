cd0b0e5e7572167e82a98a3f184528f911b2d8ec
Roy1715 <tennti0132@gmail.com>
2019-01-01 23:12:43 +0900

    2018/1/1

---
+++ ImreadTest.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as image
+
+img=image.imread('/Users/y.h/Downloads/6375fc39bf885c1ec9d99445f8720127f1f728ac.png')
+plt.imshow(img)
+plt.show()
