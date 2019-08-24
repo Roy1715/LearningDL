432d2066daf0181e37bc93115044a811d1ca9419
Roy1715 <tennti0132@gmail.com>
2019-08-24 13:49:14 +0900

    Update:Convolution.py

--- Convolution.py
+++ Convolution.py
@@ -1,6 +1,24 @@
 import numpy as np
+from SampleCode.common.util import im2col
 
 
 class Convolution:
     def __init__(self, w, b, stride=1, pad=0):
-        self.w = w
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        FN, C, FH, FW = self.w.shape
+        N, C, H, W = x.shape
+        out_h = int(1 + (H + 2 * self.pad - FH) / self.stride)
+        out_w = int(1 + (W + 2 * self.pad - FW) / self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T
+        out = np.dot(col, col_W) + self.b
+
+        out = out.reshape(N, out_h, out_w, -1).transepose(0, 3, 1, 2)
+
+        return out
